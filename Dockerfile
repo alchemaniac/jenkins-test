70bea0b8d8ea8d81b349e827f42fe8b5c3bc874d
root <root@localhost.localdomain>
2020-01-29 15:55:30 +0530

    corrected erros

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM python:3-onbuild
 
 COPY templates/style.css /usr/src/app/static/style.css
-COPY templates/js1.js /usr/src/app/static/js1.js
 COPY templates/angular.js /usr/src/app/static/angular.js
+COPY templates/js1.js /usr/src/app/static/js1.js
+
 
 EXPOSE 5000
 
--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template, request
 import mysql.connector
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-	print("Connecting to database ...")
-	try:
-		mydb = mysql.connector.connect(host="mysql",user="root",password="mysql12345")
-	except:
-		print("Connection failed!")
-	print("Preparing database ...")
+	mydb = mysql.connector.connect(host="mysql",user="root",password="mysql12345")
 	mycursor = mydb.cursor()
 	try:
 		mycursor.execute("CREATE DATABASE mydatabase")
@@ -18,24 +13,24 @@ def index():
 		print('db exists')
 	mycursor.execute("USE mydatabase")
 	try:
-		mycursor.execute("CREATE TABLE somedata (name VARCHAR(255) NOT NULL, id VARCHAR(255) NOT NULL, dob DATE NOT NULL)")
+		mycursor.execute("CREATE TABLE somedata (name VARCHAR(255) NOT NULL, id VARCHAR(255) NOT NULL,  dob VARCHAR(255) NOT NULL)")
 	except:
 		print('table exists')
-	query = """INSERT INTO somedata VALUES (%s, %s, STR_TO_DATE(%s, '%d-%m-%Y'))"""
-	print("Waiting for data ...")
-	if request.method=="POST":
-		print("Receiving data ...")
-		details=request.form
-		tname=details['name']
-		tid=details['id']
-		tdate=details['date']
-		indata=(tname, tid, tdate)
-		print("Inserting data ...")
-		mycursor.execute(query, indata)
-		mycursor.execute("commit;")
-	print("Fetching data ...")
+	query = """INSERT INTO somedata VALUES (%s, %s, %s)"""
+	try:
+		if request.method=="POST":
+			details=request.form
+			tname=details['name']
+			tid=details['id']
+			tdate=details['date']
+			indata=(tname, tid, tdate)
+			mycursor.execute(query, indata)
+			mycursor.execute("commit;")
+	except:
+		print("data insertion failure!")
 	mycursor.execute("select * from somedata")
 	result=mycursor.fetchall()
+	#pdata=""
 	pname=[]
 	pid=[]
 	pdate=[]
@@ -44,7 +39,6 @@ def index():
 			pname.append(data[0])
 			pid.append(data[1])
 			pdate.append(data[2])
-	print("Rendering data ...")
 	return render_template('index.html',pdata=zip(pname,pid,pdate))
 
 if __name__ == "__main__":
